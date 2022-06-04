a3c77f78d42caffd0483caa272a63e90c851b251
Ihor Kladko <kihoro2d@gmail.com>
2022-06-04 04:16:35 +0300

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-named-default, no-unused-vars, new-cap */
+
+import 'dotenv/config'
+
+import { Bot, InlineKeyboard } from 'grammy'
+import { default as OutlineVPN } from 'outlinevpn-api'
+
+const bot = new Bot(process.env.BOT_TOKEN)
+const outlineVpn = new OutlineVPN.default(process.env.OUTLINE_API_URL)
+
+const isAdmin = ctx => ctx.from?.id === parseInt(process.env.BOT_ADMIN, 10)
+const multiline = text => text.replace(/^[ \t]+|[ \t]+$/gm, '')
+
+bot.command('start', async (ctx) => {
+  const user = ctx.from.username || 'username'
+  const year = new Date().getFullYear()
+
+  await ctx.reply(multiline(`
+    Hello, *${user}*\\!
+    Welcome to ~${year}~ *1984*\\!
+  `), {
+    parse_mode: 'MarkdownV2',
+    reply_markup: new InlineKeyboard()
+      .webApp('Get Outline', 'https://getoutline.org/get-started/').row()
+      .webApp('Free Access Keys', 'https://outline.community/access-keys/')
+  })
+})
+
+bot.catch((err) => {
+  const ctx = err.ctx
+  console.error(`Error while handling update ${ctx.update.update_id}:`, err.error)
+})
+
+bot.start({ drop_pending_updates: true })
